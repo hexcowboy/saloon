f1c7b794e927b343dda6420c21f84bd5c21689d4
hexcowboy <hex@cowboy.dev>
2021-06-13 19:18:00 -0500

    change burp suite install with better defaults

--- scripts/install-burp-suite.sh
+++ scripts/install-burp-suite.sh
@@ -1,16 +1,31 @@
 #!/bin/bash
+# https://github.com/hexcowboy/docker-burp-suite-community/blob/master/Dockerfile
 
-DEPS="curl openjdk-8-jre"
-URL="https://portswigger.net/burp/releases/download?product=community&version=2021.5.2&type=Linux"
+DEPS="curl default-jre openssl ca-certificates libxext6 libxrender1 libxtst6"
+URL="https://portswigger.net/burp/releases/download"
+JAR_FILE="/usr/local/BurpSuiteCommunity/burpsuite_community.jar"
+BIN_FILE="/usr/local/bin/BurpSuiteCommunity"
 
 apt-get install -y $DEPS
 
-# Install the Burpsuite jar
-curl -fsSL $URL -o burp-installer.sh
+# Download the Burpsuite jar
+mkdir -p $(dirname $JAR_FILE)
+curl -fsSL $URL -o $JAR_FILE
 
-# Run the install script
-yes "" | bash burp-installer.sh
+# Create a binary
+cat << EOF > $BIN_FILE
+#!/usr/bin/env sh
+
+PROJECT_CONFIG="/etc/burpsuite/project_options.json"
+USER_CONFIG="/etc/burpsuite/user_options.json"
+
+set -e
+
+exec java -jar "$JAR_FILE" --config-file="\$PROJECT_CONFIG" --user-config-file="\$USER_CONFIG"
+EOF
+
+chmod u+x $BIN_FILE
 
 # Create symlinks
-ln -s $(which BurpSuiteCommunity) /usr/local/bin/burp
-ln -s $(which BurpSuiteCommunity) /usr/local/bin/burpsuite
+ln -s $BIN_FILE /usr/local/bin/burp
+ln -s $BIN_FILE /usr/local/bin/burpsuite
